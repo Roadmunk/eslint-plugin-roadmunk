9355ab6b157912829642ceefa82004cc093c6626
Steven McConomy <srmcconomy@gmail.com>
2018-07-23 09:50:06 -0400

    eslint and fixes from review

--- tests/lib/rules/no-lodash-deprecated-functions.js
+++ tests/lib/rules/no-lodash-deprecated-functions.js
@@ -2,41 +2,41 @@
  * @fileoverview Prevent usage of deprecated lodash methods
  * @author Steven McConomy
  */
-"use strict";
+'use strict';
 
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 // Requirements
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 
-const rule       = require("../../../lib/rules/no-lodash-deprecated-functions");
-const RuleTester = require("eslint").RuleTester;
+const rule       = require('../../../lib/rules/no-lodash-deprecated-functions');
+const RuleTester = require('eslint').RuleTester;
 
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 // Tests
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 
-const errors = [{
-	message : "Use _.includes instead of the deprecated _.contains",
-	type    : "MemberExpression"
-}];
+const errors = [ {
+	message : 'Use _.includes instead of the deprecated _.contains',
+	type    : 'MemberExpression',
+} ];
 
 const ruleTester = new RuleTester();
-ruleTester.run("no-lodash-deprecated-functions", rule, {
+ruleTester.run('no-lodash-deprecated-functions', rule, {
 	valid : [
-		"_.includes(a, list, of, arguments)",
-		"_.containsWithMoreThingsHere()",
-		"f = _.includes",
+		'_.includes(a, list, of, arguments)',
+		'_.containsWithMoreThingsHere()',
+		'f = _.includes',
 	],
 	invalid : [
 		{
-			code : "_.contains([ 1 ], 1)",
+			code   : '_.contains([ 1 ], 1)',
 			errors,
-			output : "_.includes([ 1 ], 1)",
+			output : '_.includes([ 1 ], 1)',
 		},
 		{
-			code : "f = _.contains",
+			code   : 'f = _.contains',
 			errors,
-			output : "f = _.includes",
+			output : 'f = _.includes',
 		},
 	],
 });
