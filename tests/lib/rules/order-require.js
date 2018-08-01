75cdf8f1cf7ff340e2667c59ddc951916167cfed
Watandeep Sekhon <watan.cp@gmail.com>
2018-08-01 16:30:39 -0400

    Change order & improve autofix logic

--- tests/lib/rules/order-require.js
+++ tests/lib/rules/order-require.js
@@ -23,72 +23,85 @@ ruleTester.run('order-require', rule, {
 		var lib2 = require('../lib/test');
 		var lib2 = require('common/lib/test');
 		var lib3 = require('../common/lib/test');
-		var views1 = require('views/test');
-		var views2 = require('../views/test');
-		var views2 = require('common/views/test');
-		var views3 = require('../common/views/test');
 		var models1 = require('models/test');
 		var models2 = require('../models/test');
 		var models2 = require('common/models/test');
 		var models3 = require('../common/models/test');
+		var views1 = require('views/test');
+		var views2 = require('../views/test');
+		var views2 = require('common/views/test');
+		var views3 = require('../common/views/test');
+		`,
+		`
+		var _         = require('lodash');
+		var pluralize = require('pluralize');
+		var JS        = require('@roadmunk/jsclass');
+		var Moment    = require('lib/rm-moment');
+		var BaseModel = require('models/BaseModel');
+		var Msgbox    = require('views/Msgbox');
 		`,
 	],
 
 	invalid : [
-		// 		{
-		// 			code :
-		// 			`
-		// var second = require('lib/test');
-		// var first = require('first');
-		// 			`,
-		// 			errors : [ {
-		// 				message : 'first was not required in the correct order',
-		// 				type    : 'CallExpression',
-		// 			} ],
-		// 			output :
-		// 			`
-		// var first = require('first');
-		// var second = require('lib/test');
-		// 			`,
-		// 		},
-		// 		{
-		// 			code :
-		// 			`
-		// var first = require('first');
-		// var third = require('common/lib/test');
-		// var second = require('lib/test');
-		// 			`,
-		// 			errors : [ {
-		// 				message : 'lib/test was not required in the correct order',
-		// 				type    : 'CallExpression',
-		// 			} ],
-		// 			output :
-		// 			`
-		// var first = require('first');
-		// var second = require('lib/test');
-		// var third = require('common/lib/test');
-		// 			`,
-		// 		},
 		{
 			code :
 			`
-var third = require('common/lib/test');
-var first = require('first');
-var second = require('lib/test');
+var JS = require('@roadmunk/jsclass');
+var _  = require('lodash');
 			`,
 			errors : [ {
-				message : 'first was not required in the correct order',
+				message : 'lodash was not required in the correct order',
 				type    : 'CallExpression',
 			},
-			{
-				message : 'lib/test was not required in the correct order',
-				type    : 'CallExpression',
-			} ],
+			],
 			output :
 			`
-var first = require('first');
-var second = require('lib/test');
-var third = require('common/lib/test');
+var _  = require('lodash');
+var JS = require('@roadmunk/jsclass');
+			`,
+		},
+		{
+			code :
+			`
+var _  = require('lodash');
+var JS = require('@roadmunk/jsclass');
+var pluralize = require('pluralize');
+			`,
+			errors : [ {
+				message : 'pluralize was not required in the correct order',
+				type    : 'CallExpression',
+			},
+			],
+			output :
+			`
+var _  = require('lodash');
+var pluralize = require('pluralize');
+var JS = require('@roadmunk/jsclass');
+			`,
+		},
+		{
+			code :
+			`
+var _         = require('lodash');
+var pluralize = require('pluralize');
+var JS        = require('@roadmunk/jsclass');
+var Moment    = require('lib/rm-moment');
+var Msgbox    = require('views/Msgbox');
+var BaseModel = require('models/BaseModel');
+			`,
+			errors : [ {
+				message : 'models/BaseModel was not required in the correct order',
+				type    : 'CallExpression',
+			},
+			],
+			output :
+			`
+var _         = require('lodash');
+var pluralize = require('pluralize');
+var JS        = require('@roadmunk/jsclass');
+var Moment    = require('lib/rm-moment');
+var BaseModel = require('models/BaseModel');
+var Msgbox    = require('views/Msgbox');
 			`,
 		},
 	],
