a8eaeefb5c84888e07301ea294c510c8af16ad6f
Gord Tanner <gord@roadmunk.com>
2018-08-02 16:05:15 -0400

    better fixer ( and fix toLowerCase )
    - detect and fix toLowerCase compares
    - fixer now handles TemplateLiterals
    - fixer now handles CallExpressions

--- tests/lib/rules/no-string-localecompare.js
+++ tests/lib/rules/no-string-localecompare.js
@@ -17,58 +17,106 @@ const RuleTester = require('eslint').RuleTester;
 // ------------------------------------------------------------------------------
 const ruleTester = new RuleTester({ parserOptions : { ecmaVersion : 2017 } });
 
-const errors = [ {
-	message : 'please use `_.areEqualCaseless(string1, string2)` instead of `String.localeCompare()`',
-	type    : 'CallExpression',
-} ];
+const errors = [
+	{
+		message : 'please use `_.areEqualCaseless(string1, string2)` instead of `String.localeCompare()`',
+		type    : 'CallExpression',
+	},
+	{
+		message : 'please use `_.areEqualCaseless(string1, string2)` instead of `string1.toLowerCase() == string2.toLowerCase()`',
+		type    : 'BinaryExpression',
+	},
+	{
+		message : 'please use `_.areEqualCaseless(string1, string2)` instead of `string1.toLowerCase() === string2.toLowerCase()`',
+		type    : 'BinaryExpression',
+	},
+];
 
 ruleTester.run('no-string-localecompare', rule, {
 	valid : [
+		{ code : 'a.prop.localeCompare(b.prop, {})' },
+		{ code : 'a.prop.localeCompare(b.prop)' },
+		{ code : 'a.prop.localeCompare(b.prop, { awesome : true })' },
+		{ code : 'a.prop.localeCompare(b.prop, { sensitivity : "notbase" })' },
+		{ code : 'a.prop.localeCompare(b.prop, { sensitivity : "notbase" })' },
+		{ code : 'const value = arg.name.toLowerCase() !== thing.toLowerCase();' },
+		{ code : 'const value = arg.name.toLowerCase() >= thing.toLowerCase();' },
+		{ code : 'const value = arg.name.toLowerCase() <= thing.toLowerCase();' },
+		{ code : 'const value = arg.name.toLowerCase() > thing.toLowerCase();' },
+		{ code : 'const value = arg.name.toLowerCase() < thing.toLowerCase();' },
 	],
 
 	invalid : [
 		{
-			code   : 'a.prop.localeCompare(b.prop, {sensitiviy : \'base\' })',
-			errors,
+			code   : 'a.prop.localeCompare(b.prop, {sensitivity : \'base\' })',
 			output : '_.areEqualCaseless(a.prop, b.prop)',
+			errors : [ errors[0] ],
 		},
 		{
-			code   : 'a.localeCompare(\'const\', {sensitiviy : \'base\' })',
-			errors,
+			code   : 'a.localeCompare(\'const\', {sensitivity : \'base\' })',
 			output : '_.areEqualCaseless(a, \'const\')',
+			errors : [ errors[0] ],
 		},
 		{
-			code   : '\'foo\'.localeCompare(\'const\', {sensitiviy : \'base\' })',
-			errors,
+			code   : '\'foo\'.localeCompare(\'const\', {sensitivity : \'base\' })',
 			output : '_.areEqualCaseless(\'foo\', \'const\')',
+			errors : [ errors[0] ],
 		},
 		{
-			code   : 'a.prop.localeCompare(b.prop.stuff, {sensitiviy : \'base\' })',
-			errors,
+			code   : 'a.prop.localeCompare(b.prop.stuff, {sensitivity : \'base\' })',
 			output : '_.areEqualCaseless(a.prop, b.prop.stuff)',
+			errors : [ errors[0] ],
 		},
 		{
-			code : `str.localeCompare(\`one ${2} three\`, {sensitiviy : 'base' })`,
-			errors,
-			// TODO: enable this
-			// output : `_.areEqualCaseless(str, \`one ${2} three\`)`,
+			code   : 'str.localeCompare(`one ${2} three`, {sensitivity : \'base\' })',
+			output : '_.areEqualCaseless(str, `one ${2} three`)',
+			errors : [ errors[0] ],
 		},
 		{
-			code : '_.get(foo, \'bar.baz\').localeCompare(b.prop.stuff, {sensitiviy : \'base\' })',
-			errors,
-			// TODO: enable this
-			// output : `_.areEqualCaseless(_.get(foo, 'bar.baz'), b.prop.stuff)`,
+			code   : '_.get(foo, \'bar.baz\').localeCompare(b.prop.stuff, {sensitivity : \'base\' })',
+			output : '_.areEqualCaseless(_.get(foo, \'bar.baz\'), b.prop.stuff)',
+			errors : [ errors[0] ],
 		},
+		{
+			code   : '`wow`.localeCompare(_.get(obj, "stuff"), {sensitivity : \'base\' });',
+			output : '_.areEqualCaseless(`wow`, _.get(obj, "stuff"));',
+			errors : [ errors[0] ],
+		},
+		{
+			code   : 'const func = arg => arg.name.toLowerCase() == thing.toLowerCase();',
+			output : 'const func = arg => _.areEqualCaseless(arg.name, thing);',
+			errors : [ errors[1] ],
+		},
+		{
+			code   : 'const value = arg.name.toLowerCase() === thing.toLowerCase();',
+			output : 'const value = _.areEqualCaseless(arg.name, thing);',
+			errors : [ errors[2] ],
+		},
+		/*
+		{
+			code   : `!_.some(this.fieldMappingsToShow, { externalFieldName : option.id }) // field is not already added
+                                        && !_.some(this.wizard.roadmunkFields, field => // field is not hidden
+                                                this.hiddenMappings.includes(field.id)
+	                                              && (field.name.toLowerCase() === option.name.toLowerCase() || field.id.toLowerCase() === option.name.toLowerCase())
+                                        )`,
+			output : `!_.some(this.fieldMappingsToShow, { externalFieldName : option.id }) // field is not already added
+                                        && !_.some(this.wizard.roadmunkFields, field => // field is not hidden
+                                                this.hiddenMappings.includes(field.id)
+																								&& (_.areEqualCaseless(field.name, option.name) || _.areEqualCaseless(field.id, option.name))
+                                        )`,
+			errors : [ errors[0] ],
+		},
+		*/
 		{
 			code : `const usedBy = _([ _.get(this.usageCountMetadata, 'roadmaps', []), _.get(this.usageCountMetadata, 'masters', []) ])
-										.map(tables => tables.sort((a, b) => a.title.localeCompare(b.title, { sensitiviy : 'base' })))
+										.map(tables => tables.sort((a, b) => a.title.localeCompare(b.title, { sensitivity : 'base' })))
 										.flatten()
 										.value();`,
-			errors,
 			output : `const usedBy = _([ _.get(this.usageCountMetadata, 'roadmaps', []), _.get(this.usageCountMetadata, 'masters', []) ])
 										.map(tables => tables.sort((a, b) => _.areEqualCaseless(a.title, b.title)))
 										.flatten()
 										.value();`,
+			errors : [ errors[0] ],
 		},
 	],
 });
